da0a18e8a7b0cc0f0a219f2d2558f8cf6357c910
QuLusche <quenquenl.jeux@gmail.com>
2025-09-07 21:47:04 +0200

    create plugin project

---
+++ src/main/java/fr/molequle/qulusche/Vesoul.java
@@ -0,0 +1,15 @@
+package fr.molequle.qulusche;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Vesoul extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
