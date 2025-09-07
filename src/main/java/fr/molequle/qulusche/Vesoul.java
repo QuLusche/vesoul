2f22b7d4407779194b0634f628c68f4a1ae88346
QuLusche <quenquenl.jeux@gmail.com>
2025-09-07 22:04:21 +0200

    add FoliaLib

--- src/main/java/fr/molequle/qulusche/Vesoul.java
+++ src/main/java/fr/molequle/qulusche/Vesoul.java
@@ -1,15 +1,30 @@
 package fr.molequle.qulusche;
 
+import com.tcoded.folialib.FoliaLib;
+import lombok.Getter;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Vesoul extends JavaPlugin {
 
+	public static Plugin plugin;
+
+	@Getter
+	private FoliaLib foliaLib;
+
 	@Override
 	public void onEnable() {
+		plugin = this;
+		foliaLib = new FoliaLib(this);
 
+		getLogger().info("Vesoul plugin enabled");
 	}
 
 	@Override
 	public void onDisable() {
+		plugin = null;
+		this.foliaLib.getScheduler().cancelAllTasks();
+
+		getLogger().info("Vesoul plugin disabled");
 	}
 }
